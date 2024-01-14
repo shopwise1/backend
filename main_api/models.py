1a6b8f2c1cd777dccc823d9dd5c44b703489cb10
Re1gns <lreigns12@gmail.com>
2024-01-14 22:17:31 +0100

    product model

--- main_api/models.py
+++ main_api/models.py
@@ -8,3 +8,19 @@ class Vendor(models.Model):
     vendor = models.ForeignKey(User, on_delete=models.CASCADE)
     address = models.TextField(null=True)
 
+#Products Category
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+    
+#Products
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
